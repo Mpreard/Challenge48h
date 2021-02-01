db99a9b9e0ce98bcef1dbe77c08b627a6f8b6230
Maxime <maxime.preard@ynov.com>
2021-02-01 12:51:41 +0100

    Add link bootstrap and jquery

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<link href="./bootstrap-5.0.0-beta-dist/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+<script type="text/javascript" src="./bootstrap-5.0.0-beta-dist/js/bootstrap.js"></script>
+<script type="text/javascript" src="./bootstrap-5.0.0-beta-dist/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="./layout/js/jquery.js"></script>
+</html>
