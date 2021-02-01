5318c74e1d1529e8c22145750db423bc97495817
bnbnbn1812 <60342847+bnbnbn1812@users.noreply.github.com>
2021-02-01 17:19:10 +0100

    Update rubikscube.php

--- php/rubikscube.php
+++ php/rubikscube.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link type="text/css" rel="stylesheet" href="layout/css/rubikscube.css">
+    <script type="text/javascript" src="layout/js/rubikscube.js">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
