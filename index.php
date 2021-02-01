0e742366513ede9c6f0ec5beb9afdf57a862a164
Maxime PREARD <maxime.preard@vigicorp.fr>
2021-02-01 13:45:42 +0100

    fix bootstrap problem

--- index.php
+++ index.php
@@ -2,15 +2,35 @@
 <html>
 <head>
 <title>Page Title</title>
-<link href="./bootstrap-5.0.0-beta-dist/css/bootstrap.css" rel="stylesheet">
+<link href="layout/bootstrap/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
 
 <h1>My First Heading</h1>
 <p>My first paragraph.</p>
-
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
 </body>
-<script type="text/javascript" src="./bootstrap-5.0.0-beta-dist/js/bootstrap.js"></script>
-<script type="text/javascript" src="./bootstrap-5.0.0-beta-dist/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="./layout/js/jquery.js"></script>
+<script type="text/javascript" src="layout/bootstrap/js/bootstrap.js"></script>
+<script type="text/javascript" src="layout/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="layout/js/jquery.js"></script>
 </html>
