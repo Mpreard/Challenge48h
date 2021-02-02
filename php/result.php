7f14d3f1a4a1562ce81626fba40efc230e90aaca
Mathis <mathis.veron@ynov.com>
2021-02-02 14:17:53 +0100

    correction de bug
    correction de bug

--- php/result.php
+++ php/result.php
@@ -9,7 +9,7 @@ if(empty($user_answer)){
 
 if($user_answer === 'JRP1DUELP4ZK'){
     header ("Content-type: image/jpeg");
-    $image = imagecreatefromjpeg("../layout/img/victoire.jpg");
+    $image = imagecreatefromjpeg("victoire.jpg");
     imagejpeg($image);
 
 } else {
