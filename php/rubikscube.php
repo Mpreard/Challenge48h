a2d212a64c34c2ec79c3d7795981afb2f4655c64
bnbnbn1812 <60342847+bnbnbn1812@users.noreply.github.com>
2021-02-02 10:53:23 +0100

    Rubik's cube
    affichage du cube

--- php/rubikscube.php
+++ php/rubikscube.php
@@ -1,15 +1,37 @@
+<?php  
+
+session_start();
+
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link type="text/css" rel="stylesheet" href="layout/css/rubikscube.css">
-    <script type="text/javascript" src="layout/js/rubikscube.js">
+    <link rel="stylesheet" href="../layout/css/rubikscube.css">
+    <script type="text/javascript" src="../layout/js/rubikscube.js"></script>
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+<div id="wrapD3Cube">
+    <div id="D3Cube">
+        <div id="side1"></div>
+        <div id="side2"></div>
+        <div id="side3"></div>
+        <div id="side4"></div>
+        <div id="side5"></div>
+        <div id="side6"></div>
+    </div>
+</div>
+
+    <a onclick="turnLeft()">Left</a>
+    <a onclick="turnRight()">Right</a> <br />
+    <a onclick="flipCube()">Flip</a>
+
 </body>
+
+
 </html>
-<?php 
 
